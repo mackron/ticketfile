84f90c8142207e8cfc15ec2a000e675403a43539
David Reid <mackron@gmail.com>
2026-08-13 05:45:26 +1000

    Version 1.1.0.

--- source/ticketfile_version.h
+++ source/ticketfile_version.h
@@ -2,7 +2,7 @@
 #define ticketfile_version_h
 
 #define TICKETFILE_VERSION_MAJOR  1
-#define TICKETFILE_VERSION_MINOR  0
+#define TICKETFILE_VERSION_MINOR  1
 #define TICKETFILE_VERSION_PATCH  0
 
 #endif  /* ticketfile_version_h */
